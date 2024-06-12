d258b32ef003b955892a4ec68675b15c438a0d2a
Junsang Mun <mun.js@aol.com>
2024-06-12 15:41:24 +0900

    [feat] passed exam04

--- fuck exam04/test.c
+++ fuck exam04/test.c
@@ -1,6 +1,6 @@
 #include <string.h>
-#include <sys/wait.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 int err(char *str) {
   while (*str)
@@ -9,47 +9,47 @@ int err(char *str) {
 }
 
 int cd(char **argv, int i) {
-  if (i > 2)
-    return err("error: cd: bad arguments\n");
+  if (i != 2)
+    return err("error: cd:");
   if (chdir(argv[1]) == -1)
-    return err("error: cd: cannot change directory to "), err(argv[1]),
-           err("\n");
+    return err("error: cd: cannot change directory to "), err(argv[1]);
   return 0;
 }
 
 int exec(char **argv, char **envp, int i) {
-  int has_pipe = 0;
   int fd[2];
-  int status = 0;
+  int status;
+  int has_pipe = argv[i] && !strcmp(argv[i], "|");
 
   if (!has_pipe && !strcmp(*argv, "cd"))
     return cd(argv, i);
-
-  if (argv[i] && !strcmp(argv[i], "|"))
-    has_pipe = 1;
+  
   if (has_pipe && pipe(fd) == -1)
     return err("error: fatal\n");
+
   int pid = fork();
   if (!pid) {
     argv[i] = 0;
-    if (has_pipe &&
-        (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+
+    if (has_pipe && (dup2(fd[1], 1) == -1|| close(fd[0]) == -1 || close(fd[1]) == -1 ))
       return err("error: fatal\n");
+    
     if (!strcmp(*argv, "cd"))
       return cd(argv, i);
+    
     execve(*argv, argv, envp);
-    return err("error: cannot execute "), err(*argv), err("\n");
+    return err("error: cannot execute");
   }
   waitpid(pid, &status, 0);
-  if (has_pipe &&
-      (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-    return err("error: fatal\n");
+  if (has_pipe && (dup2(fd[0], 0) == -1|| close(fd[0]) == -1 || close(fd[1]) == -1 ))
+      return err("error: fatal\n");
   return WIFEXITED(status) && WEXITSTATUS(status);
 }
 
 int main(int argc, char **argv, char **envp) {
   int i = 0;
   int status = 0;
+
 
   if (argc > 1) {
     while (argv[i] && argv[++i]) {
